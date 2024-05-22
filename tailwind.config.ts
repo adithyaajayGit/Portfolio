6b5efbda6ad8a45da6b16981b8b2c8437a8f0260
Adithya Ajay <adithyaajay098@gmail.com>
2024-05-22 10:16:30 +0530

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import { DEFAULT_CIPHERS } from "tls";
 
 const config: Config = {
   content: [
@@ -8,11 +9,10 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+     colors:{
+      DEFAULT: '#000',
+      100: '#000319'
+     }
     },
   },
   plugins: [],
