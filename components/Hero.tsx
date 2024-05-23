1f6cfa13447ad53ce57175a41baff90025c02ebc
Adithya Ajay <adithyaajay098@gmail.com>
2024-05-24 00:57:14 +0530

    small text on top added

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -9,12 +9,16 @@ const Hero = () => {
                 <Spotlight className='top-10 left-full  h-[80vh] w-[50vw]' fill='purple' />
                 <Spotlight className='top-28 left-80 h-[80vh] w-[50vw] ' fill='blue' />
             </div>
-            <div>
-                <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.3] bg-grid-black/[0.2] relative flex items-center justify-center">
+                <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.03] bg-grid-black/[0.2]  flex items-center justify-center absolute top-0 left-0">
                     <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>      
                 </div>
+                <div className='flex justify-center relative my-20 '>
+                <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'></div>
+                <h2 className='uppercase tracking-widest text-xs text-center text-blue-100 max-w-80'>
+                    Dynamic wev magic with next.js
+                </h2>
+                </div>
             </div>
-        </div>
     )
 }
 
