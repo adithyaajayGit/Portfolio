7e0dd03662f375fd8623b4a0fce07023d0c06681
Adithya Ajay <adithyaajay098@gmail.com>
2024-05-28 23:15:07 +0530

    added images to pin

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -14,10 +14,17 @@ const RecentProjects = () => {
       </h1>
       <div className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10">
        
-        {projects.map(({  id, title, des, img, iconLists, Link }) => (
+        {projects.map(({  id, title, des, img, iconLists, link }) => (
             <div key={id} className='lg:min-h-[32.5 rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]'>
-               <PinContainer>
-                {title}
+               <PinContainer title={title} href={link}>
+                <div className='relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh]'>
+                  <div>
+                    <img src="/bg.png" alt='bg-img'/>
+                  </div>
+                  <img src={img} alt={title}
+                  className='z-10 absolute bottom-0'
+                  />
+                </div>
                </PinContainer>
             </div>
         ))}
