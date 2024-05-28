8aa04b54474fdcac6c35eb0dae6185f0cd114ecb
Adithya Ajay <adithyaajay098@gmail.com>
2024-05-28 23:05:22 +0530

    added 3d-pin

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -1,4 +1,9 @@
+import { projects } from '@/data'
+import { describe } from 'node:test'
+import { title } from 'process'
 import React from 'react'
+import { div } from 'three/examples/jsm/nodes/Nodes.js'
+import { PinContainer } from './ui/3d-pin'
 
 const RecentProjects = () => {
   return (
@@ -8,6 +13,14 @@ const RecentProjects = () => {
      <span className='text-purple'>recent projects</span>
       </h1>
       <div className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10">
+       
+        {projects.map(({  id, title, des, img, iconLists, Link }) => (
+            <div key={id} className='lg:min-h-[32.5 rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]'>
+               <PinContainer>
+                {title}
+               </PinContainer>
+            </div>
+        ))}
 
       </div>
     </div>
