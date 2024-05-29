dbac5addc270ccf9750dbb8735d9cf55c6379d8f
Adithya Ajay <adithyaajay098@gmail.com>
2024-05-29 23:31:58 +0530

    certificate section started

---
+++ components/Clients.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Clients = () => {
+    return (
+        <div className="py-20" id='projects'>
+            <h1 className='heading'>
+                Kind words from {' '}
+                <span className='text-purple'> satisfied clients</span>
+            </h1>
+            <div className="flex flex-col items-center">
+                
+            </div>
+        </div>
+    )
+}
+
+export default Clients
