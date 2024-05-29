e84ee4c36d97c8115e1cd772f8909168017311af
Adithya Ajay <adithyaajay098@gmail.com>
2024-05-29 23:44:21 +0530

    addedd infinite moving cards

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { InfiniteMovingCards } from './ui/InfiniteMovingCards'
+import { testimonials } from '@/data'
 
 const Clients = () => {
     return (
@@ -8,7 +10,13 @@ const Clients = () => {
                 <span className='text-purple'> satisfied clients</span>
             </h1>
             <div className="flex flex-col items-center">
-                
+                <div className='h-[50vh] md:h-[30rem] rounded-md flex flex-col antialiased items-center relative overflow-hidden'>
+                    <InfiniteMovingCards
+                    items={testimonials}
+                    direction='right'
+                    speed='slow'   
+                    />
+                </div>
             </div>
         </div>
     )
