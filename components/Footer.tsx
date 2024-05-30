6c77246f6827141c9c663cb44e09694804de5456
Adithya Ajay <adithyaajay098@gmail.com>
2024-05-30 11:37:14 +0530

    footer section started

---
+++ components/Footer.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='w-full pt-20 pb-10 ' id="contact">
+      <div className='w-ful'>
+        <img src="/footer-grid.svg" 
+        alt="grid" 
+        className='w-full h-full opacity-50'
+
+        
+        />
+        
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
