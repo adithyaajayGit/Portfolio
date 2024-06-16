2f6a33353f98d714c9a8a8d6eb0add585e9fa980
Adithya Ajay <adithyaajay098@gmail.com>
2024-06-16 21:48:29 +0530

    add: download

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -24,7 +24,7 @@ const Footer = () => {
     </div>
     <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
       <p className="md:text-base text-sm md:font-normal font-light">
-        Copyright © 2024 Adithya Ajay
+        {/* Copyright © 2024 Adithya Ajay */}
       </p>
 
       <div className="flex items-center md:gap-3 gap-6">
