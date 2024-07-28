2695c7798acaa4dae3bac2a3a153734bb0729f84
Adithya Ajay <adithyaajay098@gmail.com>
2024-07-29 00:50:30 +0530

    update:hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -23,7 +23,7 @@ const Hero = () => {
                 </p>
                 <TextGenerateEffect
                 className="text-center text-[40px] md:text-5xl lg:text-6xl"
-                words="  Hi Welcome I&apos;m Adithya Ajay  MERN Stack Developer "
+                words="  Hi Welcome I&apos;m Adithya Ajay  Full Stack Developer "
                 />
                 <p className='text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl'>
                 I&apos;m  a MERN Stack Developer based in Kerala, India. I specialize in building scalable and robust web applications.
